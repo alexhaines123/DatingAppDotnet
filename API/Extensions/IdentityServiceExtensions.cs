614c10e0a287608889657d5de052bce7c0cd2cab
Alex <alex.haines123@gmail.com>
2023-09-22 11:24:28 +0100

    implement auth token service, login, register

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API;
+
+public static class IdentityServiceExtensions
+{
+  public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+  {
+    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(config["TokenKey"])),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+      };
+    });
+
+    return services;
+  }
+}
